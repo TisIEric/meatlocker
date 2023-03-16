ad40ee69d87f1f2462485d3ab809c720423f1e53
Eric <89046034+TisIEric@users.noreply.github.com>
2023-03-15 20:57:47 -0400

    temprature rework
    this is simpler

--- meatlockergame.py
+++ meatlockergame.py
@@ -8,22 +8,23 @@ def intro():
     print(" _______  __   __  _______                                                     \n|       ||  | |  ||       |                                                    \n|_     _||  |_|  ||    ___|                                                    \n  |   |  |       ||   |___                                                     \n  |   |  |       ||    ___|                                                    \n  |   |  |   _   ||   |___                                                     \n  |___|  |__| |__||_______|                                                    \n               _______  ______    _______  _______  _______  _______  ______   \n              |       ||    _ |  |       ||       ||       ||       ||    _ |  \n              |    ___||   | ||  |    ___||    ___||____   ||    ___||   | ||  \n              |   |___ |   |_||_ |   |___ |   |___  ____|  ||   |___ |   |_||_ \n              |    ___||    __  ||    ___||    ___|| ______||    ___||    __  |\n              |   |    |   |  | ||   |___ |   |___ | |_____ |   |___ |   |  | |\n              |___|    |___|  |_||_______||_______||_______||_______||___|  |_|")
     return 
 
+#conversion fucntoins because
 def RtoF(oldtemp):
     return oldtemp + 460
 def FtoR(oldtemp):
     return oldtemp - 460
-
+#prettyfiying
 
 #inventory variable
 inventory = []
 #Fear variable. Fear effects what options you have.
 Fear = 0
 #internal temprature variable. You can only get so cold
-IntTemp = 558
+IntTemp = 558.0
 #external temprature variable. Dicides weather you get cold or not.
-temp = 497
-#cold resistance varable. The lower it is, the faster you get cold
-coldRes = 0
+temp = 497.0
+#cold resistance varable. The lower it is, the faster you get cold. I suggest that you make the number nice.
+coldRes = 0.0
 
 def changeTemp():
     change = 0
@@ -31,36 +32,18 @@ def changeTemp():
     global temp
     global coldRes
     #calculates change in temprature
-
-    #if the external tempreature is bigger than Internal temprature then...
-    if temp >= IntTemp:
-        #increase internal temprature by: external temprature/10 OR 5, whichever is greater
-        if temp/10 > 5:
-           change += temp/10
-        else:
-           change += 5
-    #if the external temprature is less than Internal temprature then...
-    if temp < IntTemp:
-        #decrease internal temprature by: external temprature/20 OR 5, whichever is smaller
-        if temp/10 < 1:
-            change -= temp/10
-        else:
-            change -= 1
-
-    #heat resistance calculation
-    if change < 0 and coldRes > 0:
+    #decrease temprature if bodytemp below outside temp
+    if IntTemp > temp:
+        change += 0.1
+    #increase intrnal temprature if bodytemp above outside temp
+    if IntTemp < temp:
+        change -= 0.1
+    #cold resistance variable minimises the amount of heat you loose
+    if coldRes != 0:
         change = change/coldRes
-    
-    #im rounding
-    change = round(change, 1)
-    #actually change the temprature
-    IntTemp += change
+    #rounding so that numpers dont look like 0.0000000000000000000000001
+    round(change, 3)
+    IntTemp -= change
 
 
 #DEBUG--DEBUG--DEBUG
-i = 0
-
-while i < 5:
-    changeTemp()
-    print(IntTemp)
-    i += 1
